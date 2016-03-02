b0a6ec7150a2af5a1c18e78cb84b4c0058aaeb4f
Niels van Mourik <niels.vanmourik@acquia.com>
2016-03-02 12:30:15 +0100

    Adding our PageCacheWrapper scaffold.

--- src/PurgeQueuerUrlServiceProvider.php
+++ src/PurgeQueuerUrlServiceProvider.php
@@ -11,7 +11,7 @@ use Drupal\Core\DependencyInjection\ServiceModifierInterface;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 
 /**
- *
+ * Wraps "http_middleware.page_cache" in our URL gathering substitute.
  */
 class PurgeQueuerUrlServiceProvider implements ServiceModifierInterface {
 
@@ -20,6 +20,13 @@ class PurgeQueuerUrlServiceProvider implements ServiceModifierInterface {
    */
   public function alter(ContainerBuilder $container) {
     // $container->removeDefinition('purge_queuer_url.queuer');
+
+      // http_middleware.purge_queuer_url_page_cache_wrapper:
+      //   class: Drupal\purge_queuer_url\StackMiddleware\PageCacheWrapper
+      //   arguments: ['@cache.render', '@page_cache_request_policy', '@page_cache_response_policy']
+      //   tags:
+      //     - { name: http_middleware, priority: 200, responder: true }
+
   }
 
 }
